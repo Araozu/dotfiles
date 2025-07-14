c11bfc6183ff6daa239940f3abd40917f6351a34
Fernando Araoz <fernando@araozu.dev>
2025-07-14 07:18:01 -0500

    feat: add more plugins

--- config/nvim/lua/plugins/whichkey.lua
+++ config/nvim/lua/plugins/whichkey.lua
@@ -1,13 +1,14 @@
-  return { -- Useful plugin to show you pending keybinds.
+return {
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    event = 'VimEnter',
     opts = {
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.o.timeoutlen
-      delay = 0,
+      delay = 250,
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
+        keys = {},
         -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
         -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
       },
